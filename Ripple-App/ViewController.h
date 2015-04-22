d88f042ccec63a7a77c436cd659954d22baddf1d
William O'Connor <woconnor@smu.edu>
2015-04-22 18:47:02 -0500

    first commit

---
+++ Ripple-App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ripple-App
+//
+//  Created by William O'Connor on 4/22/15.
+//  Copyright (c) 2015 Gooey Dee Bee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

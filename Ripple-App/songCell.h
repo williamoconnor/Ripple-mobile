2206fd400e564a0f41f2362bb9168c890c8bb583
William O'Connor <woconnor@smu.edu>
2015-04-22 23:16:09 -0500

    starts custom cell

---
+++ Ripple-App/songCell.h
@@ -0,0 +1,13 @@
+//
+//  songCell.h
+//  Ripple-App
+//
+//  Created by William O'Connor on 4/22/15.
+//  Copyright (c) 2015 Gooey Dee Bee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface songCell: NSObject
+
+@end
---
+++ Ripple-App/songCell.m
@@ -0,0 +1,23 @@
+//
+//  NSObject+songCell.m
+//  Ripple-App
+//
+//  Created by William O'Connor on 4/22/15.
+//  Copyright (c) 2015 Gooey Dee Bee. All rights reserved.
+//
+
+#import "songCell.h"
+#import <UIKit/UIKit.h>
+
+@interface songCell()
+
+@property (strong, nonatomic) NSDictionary* track;
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *artistLabel;
+@property (strong, nonatomic) UIImageView *albumCover;
+
+@end
+
+@implementation songCell
+
+@end
